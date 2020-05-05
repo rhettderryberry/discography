c598aa2e05c494127fbde8e2493aba2e9bed50bf
Rhett Derryberry <rhettdbvid@gmail.com>
2020-05-05 11:36:05 -0500

    Adds footer.php and nav.php

---
+++ php/footer.php
@@ -0,0 +1,9 @@
+<?php
+echo '<nav class="navbar fixed-bottom bg navbar-expand-sm">
+<div class="footer">
+   <a class="btn btn-sm"
+    href="sources.html">sources</a>
+   <p><a href="#top">Back to top</a></p>
+</div><!--footer-->
+</nav><!--navbar-->;'
+?>
---
+++ php/nav.php
@@ -0,0 +1,27 @@
+<?php
+echo '<nav class="navbar bg-warning navbar-light navbar-expand-sm sticky-top">
+  <div class="container">
+     <a class="navbar-brand" href="#">
+        <img src="images/logo.jpg" style="width:80px;" alt="logo"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button><!--navbar toggler-->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="navbar-nav ml-auto">
+          <a class="nav-item nav-link ml-4 font-weight-bolder active" href="#">Home</a>
+          <a class="font-weight-bold nav-item nav-link ml-4" href="#about">About</a>
+          <a class="font-weight-bold nav-item nav-link ml-4" href="#band">Band</a>
+          <a class="font-weight-bold nav-item nav-link ml-4" href="#discography">Discography</a>
+          <a class="font-weight-bold nav-item nav-link ml-4" href="#newsletter">NewsLetter</a>
+      </div><!--navbar-nav-->
+    </div><!--navbar-collapse-->
+  </div><!--container-->
+  </nav><!--nav-->
+  <nav class="navbar bg-success navbar-dark navbar-expand-sm">
+    <div class="container">
+      <a class="navbar-brand mx-auto">
+      <img src="images/logo1.png" style="max-width:300px;" alt="logo">
+      </a>
+    </div><!--container-->
+  </nav><!--nav-->';
+?>
