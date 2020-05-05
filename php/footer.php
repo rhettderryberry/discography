acd3b5809a044a39fca3e78bbc39184504892859
Rhett Derryberry <rhettdbvid@gmail.com>
2020-05-05 12:11:46 -0500

    Corrects typo on footer.php

--- php/footer.php
+++ php/footer.php
@@ -5,5 +5,5 @@ echo '<nav class="navbar fixed-bottom bg navbar-expand-sm">
     href="sources.html">sources</a>
    <p><a href="#top">Back to top</a></p>
 </div><!--footer-->
-</nav><!--navbar-->;'
+</nav><!--navbar-->';
 ?>
